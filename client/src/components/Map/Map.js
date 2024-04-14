7500615b4c19c3a6f11b8ac478badbb22e364e28
aaronbenham <aaron.benham123@gmail.com>
2024-04-14 19:14:23 +0100

    css changes, plus add most functionality to the page

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import H from '@here/maps-api-for-javascript';
 
 const Map = (props) => {
@@ -7,15 +7,37 @@ const Map = (props) => {
   
   var route = []
   var clickPoints = []
+  var polylines = []
+  var distances = []
+  var routeDistance = 0
   
   const platform = new H.service.Platform({
     'apikey': 'da2TME2OhQPR19NeeogV8SmFqXsGDK6SXPuUEbt93hs'
   });
 
-  console.log(props.startingPoint)
+
+  function calcTotalDistance(distanceList) {
+    var routeDistanceDiv = document.getElementById("RouteDistanceDiv")
+
+    let sum = 0;
+    for (let i = 0; i < distanceList.length; i++ ) {
+      sum += distanceList[i];
+    }
+
+    routeDistanceDiv.innerHTML = sum + "m";
+
+    if (sum >= 1000) {
+      routeDistanceDiv.innerHTML = (sum / 1000).toFixed(2) + "km";
+    } else {
+      routeDistanceDiv.innerHTML = sum + "m";
+    }
+    return (sum)
+  }
 
   useEffect(() => {
-    const clearButton = document.getElementById("TESTER")
+    const clearButton = document.getElementById("ClearRouteButton")
+    const undoButton = document.getElementById("UndoRouteButton")
+    const saveRun = document.getElementById("SaveRun")
 
     if (!map.current) {
       const defaultLayers = platform.createDefaultLayers();
@@ -30,24 +52,69 @@ const Map = (props) => {
       );
 
       const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map.current));
-
       var ui = H.ui.UI.createDefault(map.current, defaultLayers, 'en-US');
       
-      // CLEAR BUTTON PRESSED
+      // CLEAR ROUTE
       clearButton.addEventListener("click", function (evt) {
-        console.log("HELLO")
         route = []
-        console.log(clickPoints)
-  
+        distances = []
+        routeDistance = calcTotalDistance(distances)
         while(clickPoints.length > 0) { map.current.removeObject(clickPoints[0]); clickPoints.shift()}
+        while(polylines.length > 0) { map.current.removeObject(polylines[0]); polylines.shift()}
       })
 
+      // UNDO LAST POINT
+      undoButton.addEventListener("click", function (evt) {
+        distances.pop()
+        routeDistance = calcTotalDistance(distances)
+        if (clickPoints.length > 0) {
+          map.current.removeObject(clickPoints[clickPoints.length - 1])
+          clickPoints.pop()
+        }
+        if (polylines.length > 0) {
+          map.current.removeObject(polylines[polylines.length - 1])
+          polylines.pop()
+        }
+        route.pop()
+      })
+
+      saveRun.addEventListener("click", function (evt) {
+        // REFERENCE: https://downhilltodowntown.com/how-to-calculate-your-caloric-burn-while-running/
+        
+        var MET = 0 // Metabolic Equivalent of Task (MET) is a unit used to estimate the energy expenditure of various activities
+        let weight = document.getElementById("weightInput").value
+        let time = document.getElementById("timeInput").value
+
+        let avgKmPerHour = (routeDistance / 1000) / (time / 60)
+
+        const speedRanges = [4.0, 5.7, 8.0, 9.7, 11.3, 12.9, 14.5];
+        const mets = [0.5, 2, 4, 6, 8, 10, 11.5, 12.8];
+        
+        for (let i = 0; i < speedRanges.length; i++) {
+            if (avgKmPerHour < speedRanges[i]) {
+                MET = mets[i];
+                break;
+            }
+        }
+        if (MET === undefined) {
+            MET = mets[mets.length - 1];
+        }
+
+        // If avgKmPerHour is greater than or equal to the last speed range
+        var estCaloriesBurntDiv = document.getElementById("estCaloriesBurntDiv")
+        
+        // Calories Burned = MET x Body Weight (kg) x Duration of Running (hours)
+        let estCaloriesBurnt = MET * weight * (time / 60)
+        estCaloriesBurntDiv.innerHTML = routeDistance > 0 ? estCaloriesBurnt : 0
+      })
+
+      
       // MOUSE CLICK ON MAP
       map.current.addEventListener('tap', function (evt) {
         // gets coords of mouse click
         var coord = map.current.screenToGeo(evt.currentPointer.viewportX, evt.currentPointer.viewportY);
-        route.push(coord) 
-        
+        route.push(coord)
+
         // adds circle on coords
         var clickPoint = new H.map.Circle(
           new H.geo.Point(coord.lat, coord.lng),
@@ -62,6 +129,7 @@ const Map = (props) => {
         );
         clickPoints.push(clickPoint)
         map.current.addObject(clickPoint);
+        // clickPoint.addEventListener('tap', console.log("tap detected"));
 
         // once there are more than two points, connect the route
         if (map.current && route.length > 1) {
@@ -71,16 +139,44 @@ const Map = (props) => {
               transportMode: 'pedestrian',
               origin: `${route[(route.length-2)].lat},${route[(route.length-2)].lng}`,
               destination: `${route[(route.length-1)].lat},${route[(route.length-1)].lng}`,
-              return: 'polyline'
+              return: 'polyline,turnByTurnActions,actions,instructions,travelSummary'
             };
     
           routingService.calculateRoute(routeRequestParams,
             (result) => {
               if (result.routes.length) {
+                distances.push(result.routes[0].sections[0].travelSummary.length)
+                routeDistance = calcTotalDistance(distances)
+
                 result.routes[0].sections.forEach((section) => {
                   let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
                   let polyline = new H.map.Polyline(linestring, { style: { lineWidth: 4 } });
                   map.current.addObject(polyline);
+                  polylines.push(polyline)
+
+                  // testing new
+                  // var nodeOL = document.createElement('ol');
+                  // var routeInstructionsContainer = document.getElementById('panel');
+
+                  // nodeOL.style.fontSize = 'small';
+                  // nodeOL.style.marginLeft ='5%';
+                  // nodeOL.style.marginRight ='5%';
+                  // nodeOL.className = 'directions';
+
+                  // section.actions.forEach((action, idx) => {
+                  //   var li = document.createElement('li'),
+                  //       spanArrow = document.createElement('span'),
+                  //       spanInstruction = document.createElement('span');
+
+                  //   spanArrow.className = 'arrow ' + (action.direction || '') + action.action;
+                  //   spanInstruction.innerHTML = section.actions[idx].instruction;
+                  //   li.appendChild(spanArrow);
+                  //   li.appendChild(spanInstruction);
+
+                  //   nodeOL.appendChild(li);
+                  // });
+
+                  // routeInstructionsContainer.appendChild(nodeOL);
                 });
               }
             },
@@ -96,37 +192,6 @@ const Map = (props) => {
   }, []);
 
   window.addEventListener('resize', () => map.current.getViewPort().resize());
-
-
-  // useEffect(() => {
-  //   console.log(route)
-  //   if (map.current && route.length > 1) {
-  //     console.log(route[route])
-  //     const routingService = platform.getRoutingService(null, 8),
-  //       routeRequestParams = {
-  //         routingMode: 'fast',
-  //         transportMode: 'pedestrian',
-  //         origin: `${route[(route-2)].lat},${route[(route-2)].lng}`,
-  //         destination: `${route[(route-1)].lat},${route[(route-2)].lng}`,
-  //         return: 'polyline'
-  //       };
-
-  //     routingService.calculateRoute(routeRequestParams,
-  //       (result) => {
-  //         if (result.routes.length) {
-  //           result.routes[0].sections.forEach((section) => {
-  //             let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
-  //             let polyline = new H.map.Polyline(linestring, { style: { lineWidth: 4 } });
-  //             map.current.addObject(polyline);
-  //           });
-  //         }
-  //       },
-  //       (error) => {
-  //         console.error(error);
-  //       }
-  //     );
-  //   }
-  // }, [route]);
 
   return (
     <div style={{ width: "100%", height: "500px" }} ref={mapRef} />
--- client/src/pages/Routes/RoutePlanner.jsx
+++ client/src/pages/Routes/RoutePlanner.jsx
@@ -1,21 +1,88 @@
 import Map from "../../components/Map/Map";
 import { useState } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 
 const RoutePlanner = () => {
-  const [startingPoint, setStartingPoint] = useState('');
-
-  const handleInputChange = (event) => {
-    setStartingPoint(event.target.value);
-  };
-
+  // const [startingPoint, setStartingPoint] = useState('');
+  const [weight, setWeight] = useState(0)
+  const [time, setTime] = useState(0)
 
   return (
-    <div>
-      <input type="text" value={startingPoint} onChange={handleInputChange} />
-      <input id="TESTER" type="button" />
-      <Map startingPoint={startingPoint} setStartingPoint={setStartingPoint} />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Dashboard</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Monthly View
+              </Card.Subtitle>
+              <div className="stats mt-3">
+                <Row className="text-center">
+                  <Col xs={6} className="mb-4">
+                    <div className="stat-value font-weight-bold display-4">{0}</div>
+                    <div className="stat-label">Runs</div>
+                  </Col>
+                  <Col xs={6} className="mb-4">
+                    <div className="stat-value font-weight-bold display-4">{0}km</div>
+                    <div className="stat-label">Total Distance</div>
+                  </Col>
+                </Row>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>
+          <Map/>
+        </Col>
+        <Col>
+          <Row>
+            <div id="panel"></div>
+          </Row>
+          <Row>
+            {/* <input type="text" value={startingPoint} onChange={(e) => setStartingPoint(e.target.value)} /> */}
+            <Col>
+              <Button id="UndoRouteButton">Undo</Button>
+            </Col>
+            <Col>
+              <Button id="ClearRouteButton">Clear</Button>
+            </Col>
+            
+            <p>Set Weight</p>
+            <input id="weightInput" type="number" value={weight} onChange={(e) => setWeight(e.target.value)}/>
+
+            <p>Set Time</p>
+            <input id="timeInput" type="number" value={time} onChange={(e) => setTime(e.target.value)}/>
+
+            <button id="SaveRun">Save Run</button>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+      <Card>
+        <Card.Body>
+          <Card.Title>Current Run</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Stats</Card.Subtitle>
+          <div className="stats mt-3">
+            <Row className="text-center">
+              <Col xs={6} className="mb-4">
+                <div id= "RouteDistanceDiv" className="stat-value font-weight-bold display-4">0m</div>
+                <div className="stat-label">Route Distance</div>
+              </Col>
+              <Col xs={6} className="mb-4">
+                <div id="estCaloriesBurntDiv" className="stat-value font-weight-bold display-4">{0}</div>
+                <div className="stat-label">Estimated Calories Burnt</div>
+              </Col>
+            </Row>
+          </div>
+        </Card.Body>
+      </Card>
+      </Row>
+    </Container>
+
   );
 };
 
