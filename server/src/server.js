ffe996b12b0b33456eefd4c06a550c6e6ace118f
Basel Chaudry <baselchaudry@gmail.com>
2024-05-16 16:56:50 +0100

    fixing mere conflics with server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const authRoutes = require('./routes/auth');
 const workoutRoutes = require('./routes/workouts');
 const exercisesRoutes = require('./routes/exercises');
 const caloriesRoutes = require('./routes/calories');
+const routesRoutes = require('./routes/routes');
+
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +16,7 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/workouts', workoutRoutes);
 app.use('/exercises', exercisesRoutes);
+app.use('/routes', routesRoutes);
 app.use('/calories', caloriesRoutes);
 
 const PORT = process.env.PORT || 3001;
